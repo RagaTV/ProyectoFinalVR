5436ffb26cc8e1645883b5ae75362a1c8285886b
RagaTV <203480660+RagaTV@users.noreply.github.com>
2026-05-16 18:12:12 -0600

    Cambios de color según dificultad

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class BoardController : MonoBehaviour
@@ -9,6 +10,7 @@ public class BoardController : MonoBehaviour
     public TextMeshProUGUI textoRecompensa;
     public TextMeshProUGUI textoNumCliente;
     public GameObject[] estrellasDificultad; // 3 objetos de estrella
+    
     private int clientesAtendidosHoy = 0;
     [Header("Transform de Sonido")]
     public GameObject objetoGato; 
@@ -40,11 +42,39 @@ public class BoardController : MonoBehaviour
     {
         SFXManager.Instance.PlaySFXAtPosition(SFXManager.Instance.maullidoGato, objetoGato.transform.position, 1f);
         clientesAtendidosHoy++;
+
         var recetas = GameManager.Instance.todasLasRecetas;
         RecipeData recetaElegida = recetas[Random.Range(0, recetas.Count)];
+
         int dificultadCalculada = recetaElegida.ingredientesRequeridos.Count;
         GameManager.Instance.recetaObjetivoActual = recetaElegida;
         textoNombre.text = "Receta: " + recetaElegida.nombrePocion;
+
+        int suerte = Random.Range(0, 3);
+        Color colorEstrellas = Color.white;
+
+        switch (suerte)
+        {
+            case 0: // CLARA / Normal
+                Debug.Log("Pizarra: Mostrando descripción CLARA. Estrellas BLANCAS.");
+                textoDescripcion.text = recetaElegida.descripcionClara;
+                colorEstrellas = Color.white; // Blanco brillante estándar
+                break;
+            case 1: // AMBIGUA
+                Debug.Log("Pizarra: Mostrando descripción AMBIGUA. Estrellas AZULES.");
+                textoDescripcion.text = recetaElegida.descripcionAmbigua;
+                colorEstrellas = new Color(0f, 0.7f, 1f); // Un Cian/Azul brillante muy visible
+                break;
+            case 2: // CONFUSA
+                Debug.Log("Pizarra: Mostrando descripción CONFUSA. Estrellas AMARILLAS.");
+                textoDescripcion.text = recetaElegida.descripcionConfusa;
+                colorEstrellas = Color.yellow; // Amarillo intenso
+                break;
+            default:
+                textoDescripcion.text = recetaElegida.descripcionClara;
+                colorEstrellas = Color.white;
+                break;
+        }
 
         textoDescripcion.text = elegirDescripcion(recetaElegida);
 
@@ -53,11 +83,21 @@ public class BoardController : MonoBehaviour
         textoRecompensa.text = "+" + monedas;
         textoNumCliente.text = "Cliente: #" + clientesAtendidosHoy.ToString("00");
 
-        actualizarEstrellas(dificultadCalculada);
+        actualizarEstrellas(dificultadCalculada, colorEstrellas);
     }
 
     public void SolicitarNuevaMision() 
     {
+        DayNightCycle reloj = FindObjectOfType<DayNightCycle>();
+        if (reloj == null || !reloj.jornadaActiva)
+        {
+            Debug.Log("<color=yellow>[INTERFAZ]</color> El local está cerrado. Inicia el día primero.");
+            if (SFXManager.Instance != null) {
+                SFXManager.Instance.PlaySFX(SFXManager.Instance.errorIngrediente, 0.5f);
+            }
+            return; 
+        }
+
         if (GameManager.Instance.misionEnProgreso)
         {
             Debug.Log("<color=red>[BOARD]</color> Ya tienes una receta activa. ¡Termínala primero!");
@@ -74,17 +114,6 @@ public class BoardController : MonoBehaviour
                 SFXManager.Instance.PlaySFX(SFXManager.Instance.errorIngrediente, 0.5f);
             }
             return;
-        }
-
-        if (GameManager.Instance != null && !GameManager.Instance.misionEnProgreso)
-        {
-            Debug.Log("<color=yellow>[INTERFAZ]</color> El local está cerrado. Inicia el día primero.");
-            
-            if (SFXManager.Instance != null) {
-                SFXManager.Instance.PlaySFX(SFXManager.Instance.errorIngrediente, 0.5f);
-            }
-            
-            return; 
         }
 
         GameManager.Instance.misionEnProgreso = true; 
@@ -119,11 +148,20 @@ public class BoardController : MonoBehaviour
         }
     }
 
-    void actualizarEstrellas(int cantIngredientes) {
+    void actualizarEstrellas(int cantIngredientes, Color colorElegido) {
         int numEstrellas = cantIngredientes - 1; 
 
         for (int i = 0; i < estrellasDificultad.Length; i++) {
-            estrellasDificultad[i].SetActive(i < numEstrellas);
+            if (estrellasDificultad[i] != null) {
+                estrellasDificultad[i].SetActive(i < numEstrellas);
+
+                if (i < numEstrellas) {
+                    Image imgEstrella = estrellasDificultad[i].GetComponent<Image>();
+                    if (imgEstrella != null) {
+                        imgEstrella.color = colorElegido;
+                    }
+                }
+            }
         }
     }
 }
